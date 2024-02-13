1be2a4aa185ae11a850b37155c1e18e279609acf
Agnar Kåre Hereid Spilde <s362071@oslomet.no>
2024-02-13 11:19:39 +0100

    Fjernet API key, forrige key ble pushet med uhell. Er disabled.

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 # Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
 # and rename this file to constants.py.
-APIKEY = "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"
+APIKEY = "Insert key"
